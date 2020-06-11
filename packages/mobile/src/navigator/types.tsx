f3514e65c2f600de80ae1e21ad728d76d86ef41f
aaron deruvo <aaron@celo.org>
2020-06-11 09:25:06 -0700

    Merge branch 'master' into aaronmgdr/home4.2
    # Conflicts:
    #	packages/web/src/experience/brandkit/IconsExchangePage.tsx
    #	packages/web/src/experience/content/exchange-icons.md
    #	packages/web/src/shared/navigation.tsx
    oh no

--- packages/mobile/src/navigator/types.tsx
+++ packages/mobile/src/navigator/types.tsx
@@ -6,19 +6,24 @@ import { AddressValidationType } from 'src/identity/reducer'
 import { Screens } from 'src/navigator/Screens'
 import { Recipient } from 'src/recipients/recipient'
 import { TransactionDataInput } from 'src/send/SendAmount'
-import { TransferConfirmationCardProps } from 'src/send/TransferConfirmationCard'
 import { ReviewProps } from 'src/transactions/TransactionReview'
+import { TransferConfirmationCardProps } from 'src/transactions/TransferConfirmationCard'
 
-// tslint:disable-next-line
+// Typed nested navigator params
+type NestedNavigatorParams<ParamList> = {
+  [K in keyof ParamList]: undefined extends ParamList[K]
+    ? { screen: K; params?: ParamList[K] }
+    : { screen: K; params: ParamList[K] }
+}[keyof ParamList]
+
+// tslint:disable-next-line: interface-over-type-literal
 export type StackParamList = {
   [Screens.Account]: undefined
   [Screens.Analytics]: undefined
   [Screens.BackupComplete]: undefined
   [Screens.BackupIntroduction]: undefined
   [Screens.BackupPhrase]: undefined
-  [Screens.BackupQuiz]: {
-    mnemonic: string
-  }
+  [Screens.BackupQuiz]: undefined
   [Screens.BackupSocial]: undefined
   [Screens.BackupSocialIntro]: {
     incomingFromBackupFlow: boolean
@@ -37,6 +42,7 @@ export type StackParamList = {
   }
   [Screens.Debug]: undefined
   [Screens.DollarEducation]: undefined
+  [Screens.DrawerNavigator]: undefined
   [Screens.EditProfile]: undefined
   [Screens.EnterInviteCode]: undefined
   [Screens.ErrorScreen]: {
@@ -72,6 +78,7 @@ export type StackParamList = {
     backupPhrase: string
   }
   [Screens.ImportWalletSocial]: undefined
+  [Screens.ImportContacts]: undefined
   [Screens.IncomingPaymentRequestListScreen]: undefined
   [Screens.Invite]: undefined
   [Screens.InviteReview]: {
@@ -79,12 +86,16 @@ export type StackParamList = {
   }
   [Screens.JoinCelo]: undefined
   [Screens.Language]: {
-    nextScreen?: Screens.Account
+    nextScreen?: keyof StackParamList
   }
   [Screens.Licenses]: undefined
   [Screens.OutgoingPaymentRequestListScreen]: undefined
+  [Screens.PaymentRequestUnavailable]: {
+    transactionData: TransactionDataInput
+  }
   [Screens.PaymentRequestConfirmation]: {
     transactionData: TransactionDataInput
+    addressJustValidated?: boolean
   }
   [Screens.PincodeEducation]: undefined
   [Screens.PincodeEnter]: {
@@ -99,32 +110,32 @@ export type StackParamList = {
   [Screens.PhotosEducation]: undefined
   [Screens.PhotosNUX]: undefined
   [Screens.Profile]: undefined
-  [Screens.QRCode]: undefined
-  [Screens.QRScanner]:
-    | {
-        scanIsForSecureSend?: true
-        transactionData?: TransactionDataInput
-      }
-    | undefined
+  [Screens.QRNavigator]: NestedNavigatorParams<QRTabParamList> | undefined
   [Screens.ReclaimPaymentConfirmationScreen]: {
     reclaimPaymentInput: EscrowedPayment
   }
   [Screens.RegulatoryTerms]: undefined
   [Screens.Security]: undefined
   [Screens.SelectLocalCurrency]: undefined
-  [Screens.Send]: undefined
+  [Screens.Send]:
+    | {
+        isRequest?: boolean
+      }
+    | undefined
   [Screens.SendAmount]: {
     recipient: Recipient
+    isRequest?: boolean
+    isFromScan?: boolean
   }
   [Screens.SendConfirmation]: {
     transactionData: TransactionDataInput
+    addressJustValidated?: boolean
   }
   [Screens.SetClock]: undefined
   [Screens.Settings]: undefined
   [Screens.Support]: undefined
   [Screens.SupportContact]: undefined
   [Screens.Sync]: undefined
-  [Screens.TabNavigator]: undefined
   [Screens.TransactionReview]: {
     reviewProps: ReviewProps
     confirmationProps: TransferConfirmationCardProps | ExchangeConfirmationCardProps
@@ -145,6 +156,17 @@ export type StackParamList = {
   [Screens.VerificationInterstitialScreen]: undefined
   [Screens.VerificationLearnMoreScreen]: undefined
   [Screens.VerificationLoadingScreen]: undefined
-  [Screens.VerificationSuccessScreen]: undefined
+  [Screens.OnboardingSuccessScreen]: undefined
   [Screens.WalletHome]: undefined
+}
+
+// tslint:disable-next-line: interface-over-type-literal
+export type QRTabParamList = {
+  [Screens.QRCode]: undefined
+  [Screens.QRScanner]:
+    | {
+        scanIsForSecureSend?: true
+        transactionData?: TransactionDataInput
+      }
+    | undefined
 }
--- packages/mobile/src/send/Send.tsx
+++ packages/mobile/src/send/Send.tsx
@@ -1,5 +1,6 @@
 import VerifyPhone from '@celo/react-components/icons/VerifyPhone'
 import colors from '@celo/react-components/styles/colors'
+import { StackScreenProps } from '@react-navigation/stack'
 import { throttle } from 'lodash'
 import * as React from 'react'
 import { WithTranslation } from 'react-i18next'
@@ -10,47 +11,37 @@ import CeloAnalytics from 'src/analytics/CeloAnalytics'
 import { CustomEventNames } from 'src/analytics/constants'
 import { ErrorMessages } from 'src/app/ErrorMessages'
 import { estimateFee, FeeType } from 'src/fees/actions'
-import i18n, { Namespaces, withTranslation } from 'src/i18n'
+import { Namespaces, withTranslation } from 'src/i18n'
 import ContactPermission from 'src/icons/ContactPermission'
 import { importContacts } from 'src/identity/actions'
-import { headerWithCancelButton } from 'src/navigator/Headers'
+import { ContactMatches } from 'src/identity/types'
 import { navigate } from 'src/navigator/NavigationService'
 import { Screens } from 'src/navigator/Screens'
+import { StackParamList } from 'src/navigator/types'
 import {
   filterRecipientFactory,
-  filterRecipients,
+  NumberToRecipient,
   Recipient,
-  RecipientKind,
-  RecipientWithQrCode,
+  sortRecipients,
 } from 'src/recipients/recipient'
 import RecipientPicker from 'src/recipients/RecipientPicker'
 import { recipientCacheSelector } from 'src/recipients/reducer'
 import { RootState } from 'src/redux/reducers'
 import { storeLatestInRecents } from 'src/send/actions'
-import { QRCodeIcon } from 'src/send/QRCodeIcon'
 import { SendCallToAction } from 'src/send/SendCallToAction'
 import { SendSearchInput } from 'src/send/SendSearchInput'
 import DisconnectBanner from 'src/shared/DisconnectBanner'
 import { navigateToPhoneSettings } from 'src/utils/linking'
 import { requestContactsPermission } from 'src/utils/permissions'
 
-const SEARCH_THROTTLE_TIME = 50
-const defaultRecipientPhoneNumber = '+10000000000'
-const defaultRecipientAddress = `0xce10ce10ce10ce10ce10ce10ce10ce10ce10ce10`
-
-// For alfajores-net users to be able to send a small transaction to a sample address (remove post-alfajores)
-export const CeloDefaultRecipient: RecipientWithQrCode = {
-  address: defaultRecipientAddress,
-  displayName: 'Celo Default Recipient',
-  displayId: defaultRecipientPhoneNumber,
-  kind: RecipientKind.QrCode,
-  e164PhoneNumber: defaultRecipientPhoneNumber,
-}
+const SEARCH_THROTTLE_TIME = 100
 
 interface Section {
   key: string
   data: Recipient[]
 }
+
+type FilterType = (searchQuery: string) => Recipient[]
 
 interface State {
   searchQuery: string
@@ -65,7 +56,8 @@ interface StateProps {
   numberVerified: boolean
   devModeActive: boolean
   recentRecipients: Recipient[]
-  allRecipients: Recipient[]
+  allRecipients: NumberToRecipient
+  matchedContacts: ContactMatches
 }
 
 interface DispatchProps {
@@ -76,17 +68,18 @@ interface DispatchProps {
   estimateFee: typeof estimateFee
 }
 
-type Props = StateProps & DispatchProps & WithTranslation
+type RouteProps = StackScreenProps<StackParamList, Screens.Send>
+
+type Props = StateProps & DispatchProps & WithTranslation & RouteProps
 
 const mapStateToProps = (state: RootState): StateProps => ({
   defaultCountryCode: state.account.defaultCountryCode,
   e164PhoneNumber: state.account.e164PhoneNumber,
   numberVerified: state.app.numberVerified,
-  devModeActive: state.account.devModeActive || false,
-  recentRecipients: state.account.devModeActive
-    ? [CeloDefaultRecipient, ...state.send.recentRecipients]
-    : state.send.recentRecipients,
-  allRecipients: Object.values(recipientCacheSelector(state)),
+  devModeActive: state.account.devModeActive,
+  recentRecipients: state.send.recentRecipients,
+  allRecipients: recipientCacheSelector(state),
+  matchedContacts: state.identity.matchedContacts,
 })
 
 const mapDispatchToProps = {
@@ -97,48 +90,28 @@ const mapDispatchToProps = {
   estimateFee,
 }
 
-type FilterType = (searchQuery: string) => Recipient[]
-
 class Send extends React.Component<Props, State> {
-  static navigationOptions = () => ({
-    ...headerWithCancelButton,
-    headerTitle: i18n.t('sendFlow7:sendOrRequest'),
-    headerRight: <QRCodeIcon />,
-  })
-
-  throttledSearch: (searchQuery: string) => void
-  allRecipientsFilter: FilterType
-  recentRecipientsFilter: FilterType
+  throttledSearch!: (searchQuery: string) => void
+  allRecipientsFilter!: FilterType
+  recentRecipientsFilter!: FilterType
 
   constructor(props: Props) {
     super(props)
 
     this.state = {
       searchQuery: '',
-      allFiltered: [],
-      recentFiltered: [],
+      allFiltered: sortRecipients(
+        Object.values(this.props.allRecipients),
+        this.props.matchedContacts
+      ),
+      recentFiltered: this.props.recentRecipients,
       hasGivenContactPermission: true,
     }
 
-    this.allRecipientsFilter = filterRecipientFactory(this.props.allRecipients)
-    this.recentRecipientsFilter = filterRecipientFactory(this.props.recentRecipients)
-
-    this.throttledSearch = throttle((searchQuery: string) => {
-      this.setState({
-        recentFiltered: this.recentRecipientsFilter(searchQuery),
-        allFiltered: this.allRecipientsFilter(searchQuery),
-      })
-    }, SEARCH_THROTTLE_TIME)
+    this.createRecipientSearchFilters(true, true)
   }
 
   async componentDidMount() {
-    const { recentRecipients, allRecipients } = this.props
-
-    this.setState({
-      recentFiltered: filterRecipients(recentRecipients, this.state.searchQuery, false),
-      allFiltered: filterRecipients(allRecipients, this.state.searchQuery, true),
-    })
-
     await this.tryImportContacts()
 
     // Trigger a fee estimation so it'll likely be finished and cached
@@ -153,11 +126,36 @@ class Send extends React.Component<Props, State> {
       recentRecipients !== prevPops.recentRecipients ||
       allRecipients !== prevPops.allRecipients
     ) {
-      this.setState({
-        recentFiltered: filterRecipients(recentRecipients, this.state.searchQuery, false),
-        allFiltered: filterRecipients(allRecipients, this.state.searchQuery, true),
-      })
+      this.createRecipientSearchFilters(
+        recentRecipients !== prevPops.recentRecipients,
+        allRecipients !== prevPops.allRecipients
+      )
+      // Clear search when recipients change to avoid tricky states
+      this.onSearchQueryChanged('')
     }
+  }
+
+  createRecipientSearchFilters = (updateRecentFilter: boolean, updateAllFilter: boolean) => {
+    // To improve search performance, we use these filter factories which pre-process the
+    // recipient lists to improve search performance
+    if (updateRecentFilter) {
+      this.recentRecipientsFilter = filterRecipientFactory(this.props.recentRecipients, false)
+    }
+    if (updateAllFilter) {
+      this.allRecipientsFilter = filterRecipientFactory(
+        Object.values(this.props.allRecipients),
+        true,
+        this.props.matchedContacts
+      )
+    }
+
+    this.throttledSearch = throttle((searchQuery: string) => {
+      this.setState({
+        searchQuery,
+        recentFiltered: this.recentRecipientsFilter(searchQuery),
+        allFiltered: this.allRecipientsFilter(searchQuery),
+      })
+    }, SEARCH_THROTTLE_TIME)
   }
 
   tryImportContacts = async () => {
@@ -178,32 +176,31 @@ class Send extends React.Component<Props, State> {
   }
 
   onSearchQueryChanged = (searchQuery: string) => {
-    this.props.hideAlert()
-    this.setState({
-      searchQuery,
-    })
     this.throttledSearch(searchQuery)
   }
 
   onSelectRecipient = (recipient: Recipient) => {
     this.props.hideAlert()
-    CeloAnalytics.track(CustomEventNames.send_input, {
-      selectedRecipientAddress: recipient.address,
-    })
+    const isRequest = this.props.route.params?.isRequest ?? false
 
     if (!recipient.e164PhoneNumber && !recipient.address) {
       this.props.showError(ErrorMessages.CANT_SELECT_INVALID_PHONE)
       return
     }
 
-    if (
-      (recipient.e164PhoneNumber && recipient.e164PhoneNumber !== defaultRecipientPhoneNumber) ||
-      (recipient.address && recipient.address !== defaultRecipientAddress)
-    ) {
-      this.props.storeLatestInRecents(recipient)
-    }
+    this.props.storeLatestInRecents(recipient)
 
-    navigate(Screens.SendAmount, { recipient })
+    CeloAnalytics.track(
+      isRequest
+        ? CustomEventNames.request_select_recipient
+        : CustomEventNames.send_select_recipient,
+      {
+        recipientKind: recipient.kind,
+        didQuery: this.state.searchQuery.length > 0,
+      }
+    )
+
+    navigate(Screens.SendAmount, { recipient, isRequest })
   }
 
   onPermissionsAccepted = async () => {
